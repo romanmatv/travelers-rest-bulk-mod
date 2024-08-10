428a79db6742cf54a1a282e8753067e79b18f0a3
ashton <aansag@knights.ucf.edu>
2024-08-09 23:18:01 -0400

    reformatting nothing new

--- SkipFishingMod/Plugin.cs
+++ SkipFishingMod/Plugin.cs
@@ -31,22 +31,21 @@ namespace SkipFishingMod
         [HarmonyPrefix]
         static bool LateUpdatePrefix(FishingUI __instance)
         {
-                if (__instance.content.activeInHierarchy)
-                {
-                    // Get the private slider object
-                    Slider reflectedSlider = Traverse.Create(__instance)
-                        .Field("progress")
-                        .GetValue<Slider>();
-
-                    if (reflectedSlider != null)
-                    {
-                        //Plugin.DebugLog(String.Format("LateUpdatePrefix: reflectedSlider found, value {0}", reflectedSlider.value));
-                        // Set progress slider value to 1.0 for instant completion
-                        reflectedSlider.value = 1.0f;
-        
-                    }
-                }
+            if (!__instance.content.activeInHierarchy) return true;
             
+            // Get the private slider object
+            Slider reflectedSlider = Traverse.Create(__instance)
+                .Field("progress")
+                .GetValue<Slider>();
+
+            if (reflectedSlider != null)
+            {
+                //Plugin.DebugLog(String.Format("LateUpdatePrefix: reflectedSlider found, value {0}", reflectedSlider.value));
+                // Set progress slider value to 1.0 for instant completion
+                reflectedSlider.value = 1.0f;
+    
+            }
+
             return true;
         }
     }

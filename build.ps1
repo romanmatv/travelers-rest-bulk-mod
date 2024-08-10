52d99adb605614c1bec40bcae71222bdda819527
ashton <aansag@knights.ucf.edu>
2024-08-09 23:18:12 -0400

    updated script to be easier for my testing

--- build.ps1
+++ build.ps1
@@ -1,3 +1,6 @@
+Stop-Process -Name TravellersRest -ErrorAction SilentlyContinue
+
+
 dotnet build
 cp -r .\output\Debug\netstandard2.1\rbk-*.dll .
 
@@ -5,3 +8,5 @@ $mods = Get-ChildItem -Path . -Filter rbk-*.dll;
 $mods | ForEach-Object { $mod = [System.IO.Path]::GetFileNameWithoutExtension($_.FullName); 7z a -tzip "$mod.zip" "$mod.dll" }
 
 cp -force .\rbk-tr-*.dll "C:\Program Files (x86)\Steam\steamapps\common\Travellers Rest\Windows\BepInEx\plugins\."
+
+."C:\Program Files (x86)\Steam\steamapps\common\Travellers Rest\Windows\TravellersRest.exe"

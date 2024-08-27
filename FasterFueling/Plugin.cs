c27bd99f5a39d782c1566dae171605dd787ed2d0
ashton <aansag@knights.ucf.edu>
2024-08-27 00:35:03 -0400

    Added faster buying at shops.

--- FasterFueling/Plugin.cs
+++ FasterFueling/Plugin.cs
@@ -1,6 +1,8 @@
-﻿using BepInEx;
+﻿using System.Linq;
+using BepInEx;
 using BepInEx.Configuration;
 using HarmonyLib;
+using Rewired;
 
 namespace FasterFueling
 {
@@ -9,6 +11,16 @@ namespace FasterFueling
     {
         public static Harmony _harmony;
         private static ConfigEntry<int> _fuelInputWithRightClick;
+        private static ConfigEntry<int> _shopInputWithRightClick;
+        private static ConfigEntry<int> _modGamepadHotKey;
+
+
+        private static bool ModTrigger(int PlayerId)
+        {
+            return PlayerInputs.GetPlayer(PlayerId).GetButton("RightMouseDetect")
+                   || ReInput.players.GetPlayer(PlayerId - 1).controllers.Joysticks
+                       .Any(joystick => joystick.GetButton(_modGamepadHotKey.Value));
+        }
 
         private void Awake()
         {
@@ -16,6 +28,10 @@ namespace FasterFueling
             _harmony = Harmony.CreateAndPatchAll(typeof(Plugin));
             _fuelInputWithRightClick = Config.Bind("FuelInput", "input size", 5,
                 "Change the amount of fuel to insert when selecting fuel while holding RightMouse");
+            _fuelInputWithRightClick = Config.Bind("ShopInput", "input size", 25,
+                "Change the amount of items to select in Shopping Menus while holding RightMouse");
+            _modGamepadHotKey = Config.Bind("AllInput", "keycode for button trigger", 11,
+                "Haven't mapped all buttons but L3 on Stadia controller is KeyCode 11 in the Rewire.JoyStick that is being used by TravellersRest.");
 
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
         }
@@ -32,6 +48,16 @@ namespace FasterFueling
 
             for (var i = 1; i < _fuelInputWithRightClick.Value; i++)
                 __instance.OnSlotLeftClick(1, slot);
+        }
+        
+        [HarmonyPatch(typeof(ShopElementUI), nameof(ShopElementUI.SelectClicked))]
+        [HarmonyPrefix]
+        static void ShopElementUIClick(ShopElementUI __instance)
+        {
+            if (__instance == null || !ModTrigger(1)) return;
+            
+            for (var i = 1; i < _shopInputWithRightClick.Value; i++)
+                __instance.SingleElementClicked();
         }
     }
 }
---
+++ FasterFueling/change-log.md
@@ -0,0 +1,2 @@
+# 0.0.2
+- Added feature for batch shop purchase as well.
